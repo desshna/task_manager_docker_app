bfeccbac12c2c0f24bcc51b255164cf5afb18a40
Deshna Jain <jdeshna.05@gmail.com>
2026-03-06 02:33:06 +0530

    frontend update 3

--- backend/src/main/java/com/crud/taskmanager/config/CorsConfig.java
+++ backend/src/main/java/com/crud/taskmanager/config/CorsConfig.java
@@ -1,3 +1,6 @@
+package com.crud.taskmanager.config;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
